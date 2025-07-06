d35caa1120f14ab37211f6cc444f1bb662cec607
spaghetti-lover <phungducanh2511@gmail.com>
2025-07-06 16:47:26 +0700

    feat: trigger

--- backend/db/migrations/000001_init_schema.down.sql
+++ backend/db/migrations/000001_init_schema.down.sql
@@ -1,53 +1,28 @@
--- Xóa các ràng buộc đã thêm trong file up
-ALTER TABLE Flights DROP CONSTRAINT IF EXISTS departure_before_arrival;
-ALTER TABLE Seats DROP CONSTRAINT IF EXISTS unique_flight_seat;
-
--- Drop indexes for Tickets
-DROP INDEX IF EXISTS idx_tickets_flight_id;
-DROP INDEX IF EXISTS idx_tickets_booking_id;
-
--- Drop indexes for Bookings
-DROP INDEX IF EXISTS idx_bookings_return_flight_id;
-DROP INDEX IF EXISTS idx_bookings_departure_flight_id;
-DROP INDEX IF EXISTS idx_bookings_user_id;
-
--- Drop indexes for Flights
-DROP INDEX IF EXISTS idx_flights_status;
-DROP INDEX IF EXISTS idx_flights_arrival_time;
-DROP INDEX IF EXISTS idx_flights_departure_time;
-
--- Drop TicketOwnerSnapshot table
-DROP TABLE IF EXISTS TicketOwnerSnapshot;
-
--- Drop Tickets table
+-- Drop triggers
+DROP TRIGGER IF EXISTS set_updated_at ON Users;
+DROP TRIGGER IF EXISTS set_updated_at ON News;
+DROP TRIGGER IF EXISTS set_updated_at ON Bookings;
+DROP TRIGGER IF EXISTS set_updated_at ON Tickets;
+-- Drop trigger functions
+DROP FUNCTION IF EXISTS update_user_updated_at_column;
+DROP FUNCTION IF EXISTS update_news_updated_at_column;
+DROP FUNCTION IF EXISTS update_booking_updated_at_column;
+DROP FUNCTION IF EXISTS update_ticket_updated_at_column;
+-- Drop tables (in reverse dependency order)
+DROP TABLE IF EXISTS TicketOwnerSnapshots;
 DROP TABLE IF EXISTS Tickets;
-
--- Drop Seats table
 DROP TABLE IF EXISTS Seats;
-
--- Drop Bookings table
 DROP TABLE IF EXISTS Bookings;
-
--- Drop Flights table
 DROP TABLE IF EXISTS Flights;
-
--- Drop News table
-DROP TABLE IF EXISTS News;
-
--- Drop Admins table
 DROP TABLE IF EXISTS Admins;
-
--- Drop Customers table
 DROP TABLE IF EXISTS Customers;
-
--- Drop Users table
+DROP TABLE IF EXISTS News;
 DROP TABLE IF EXISTS Users;
-
 -- Drop ENUM types
 DROP TYPE IF EXISTS user_role;
-DROP TYPE IF EXISTS ticket_status;
-DROP TYPE IF EXISTS booking_status;
-DROP TYPE IF EXISTS trip_type;
 DROP TYPE IF EXISTS gender_type;
+DROP TYPE IF EXISTS trip_type;
+DROP TYPE IF EXISTS booking_status;
+DROP TYPE IF EXISTS ticket_status;
 DROP TYPE IF EXISTS flight_status;
 DROP TYPE IF EXISTS flight_class;
